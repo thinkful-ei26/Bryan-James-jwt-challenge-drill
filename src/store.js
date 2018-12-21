4dfe550b63ad6e5023edd2067f8c3895b408c5f0
James Bell <Noxachron@gmail.com>
2018-12-20 19:04:59 -0500

    added Redux dev tools support

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import thunk from 'redux-thunk';
 import {loadAuthToken} from './local-storage';
@@ -6,6 +6,8 @@ import authReducer from './reducers/auth';
 import protectedDataReducer from './reducers/protected-data';
 import {setAuthToken, refreshAuthToken} from './actions/auth';
 import {idleReducer} from './actions/auth';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     combineReducers({
         form: formReducer,
@@ -13,8 +15,15 @@ const store = createStore(
         protectedData: protectedDataReducer,
         idle: idleReducer
     }),
-    applyMiddleware(thunk)
-);
+    composeEnhancers(applyMiddleware(thunk)));
+
+
+
+
+ 
+
+
+
 
 // Hydrate the authToken from localStorage if it exist
 const authToken = loadAuthToken();
